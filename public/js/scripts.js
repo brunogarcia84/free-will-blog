49901e4f38d82a08dc3dc5d63af3b7008569bff2
Bruno Garcia <brunogarcia84@gmail.com>
2022-12-22 10:10:50 -0300

    feat: Transition and animation for the background color of the header.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,16 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
+// Animation for the header transition and background color
 
-console.log(width, height);
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  let windowRelativeBottom =
+    document.documentElement.getBoundingClientRect().top;
+
+  if (windowRelativeBottom > -600) {
+    header.style.transition = "ease-in-out 0.4s";
+    header.style.backgroundColor = "hsla(0, 0%, 77%, 40%)";
+  } else {
+    header.style.transition = "ease-in-out 0.4s";
+    header.style.backgroundColor = "var(--baltic-sea)";
+  }
+});
